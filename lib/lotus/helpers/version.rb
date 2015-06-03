fdaf0983cd869341812fa5c0c9050113b076e368
Luca Guidi <me@lucaguidi.com>
2015-06-03 09:30:04 +0200

    Bump v0.2.0

--- lib/lotus/helpers/version.rb
+++ lib/lotus/helpers/version.rb
@@ -3,6 +3,6 @@ module Lotus
     # Define version
     #
     # @since 0.1.0
-    VERSION = '0.1.0'.freeze
+    VERSION = '0.2.0'.freeze
   end
 end
--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Lotus::Helpers::VERSION do
   it 'exposes version' do
-    Lotus::Helpers::VERSION.must_equal '0.1.0'
+    Lotus::Helpers::VERSION.must_equal '0.2.0'
   end
 end

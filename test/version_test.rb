e628e3c9465d5c9bff05e2f37f44accbc5644615
Luca Guidi <me@lucaguidi.com>
2016-10-21 09:40:01 +0200

    Bump version to v0.5.0

--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Hanami::Helpers::VERSION do
   it 'exposes version' do
-    Hanami::Helpers::VERSION.must_equal '0.4.0'
+    Hanami::Helpers::VERSION.must_equal '0.5.0'
   end
 end

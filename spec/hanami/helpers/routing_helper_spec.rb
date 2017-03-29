14c858e43ba8a0e1b451ff83b0669b43088a97de
morrme <morrme@users.noreply.github.com>
2017-03-29 13:45:30 -0500

    Create routing_helper_spec.rb

---
+++ spec/hanami/helpers/routing_helper_spec.rb
@@ -0,0 +1,25 @@
+RSpec.describe Hanami::Helpers::RoutingHelper do
+  before do
+    @template = Hanami::View::Template.new('test/fixtures/templates/full_stack/dashboard/index.html.erb', 'utf-8')
+  end
+
+  describe 'when routing constant is defined' do
+    before do
+      @view = FullStack::Views::Dashboard::Index.new(@template, {})
+    end
+
+    it 'has access to routing helper' do
+      expect(@view.routing_helper_path).to eq(%(/dashboard))
+    end
+  end
+
+  describe 'when routing constant is not defined' do
+    before do
+      @view = ViewWithoutRoutingHelper.new(@template, {})
+    end
+
+    it 'raises an exception when trying to access routing helper' do
+      expect { @view.routing_helper_path }.to raise_error(NameError)
+    end
+  end
+end
